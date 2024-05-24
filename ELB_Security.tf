53b9f89bccd10396e4584c50734e1b916975b345
Anirudh <aani.spark@gmail.com>
2024-05-24 16:52:42 +0530

    spelling Mistake

--- ELB_Security.tf
+++ ELB_Security.tf
@@ -12,8 +12,7 @@ resource "aws_security_group" "elb_security_group" {
 
 resource "aws_vpc_security_group_ingress_rule" "inbound" {
   security_group_id = aws_security_group.elb_security_group.id
-
-  cidr_ipv4 = "${aws_eip.ib.public_ip}/32"
+  cidr_ipv4 = "${aws_eip.lb.public_ip}/32"
   from_port = 443
   ip_protocol = "tcp"
   to_port = 443

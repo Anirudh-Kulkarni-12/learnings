7222e893275218d8796e49c402feb7d255b0a0a8
Anirudh <aani.spark@gmail.com>
2024-05-24 16:43:54 +0530

    Elastic ip with Security Group

---
+++ ELB_Security.tf
@@ -0,0 +1,20 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_eip" "lb" {
+  domain = "vpc"
+}
+
+resource "aws_security_group" "elb_security_group" {
+  name = "elb_security"
+}
+
+resource "aws_vpc_security_group_ingress_rule" "inbound" {
+  security_group_id = aws_security_group.elb_security_group.id
+
+  cidr_ipv4 = "${aws_eip.ib.public_ip}/32"
+  from_port = 443
+  ip_protocol = "tcp"
+  to_port = 443
+}

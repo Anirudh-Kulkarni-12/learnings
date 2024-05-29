6d3acc9ef606f3bbe16e025c42cfb66ed28558c7
Anirudh <aani.spark@gmail.com>
2024-05-29 18:29:22 +0530

    Private Route

--- routeTable.tf
+++ routeTable.tf
@@ -5,9 +5,26 @@ resource "aws_route_table" "Tap-Public-Route" {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.Tapplent_igw.id
   }
+
+  tags = {
+    Name = "Tap-Public-Route"
+  }
+}
+
+resource "aws_route_table" "Tap-Private-Route" {
+  vpc_id = aws_vpc.Tapplent.id
+
+  tags = {
+    Name = "Tap-Private-Route"
+  }
 }
 
 resource "aws_route_table_association" "public" {
   subnet_id      = aws_subnet.Public-Subnet1.id
   route_table_id = aws_route_table.Tap-Public-Route.id
+}
+
+resource "aws_route_table_association" "private" {
+  subnet_id      = aws_subnet.Private-Subnet1
+  route_table_id = aws_route_table.Tap-Private-Route.id
 }

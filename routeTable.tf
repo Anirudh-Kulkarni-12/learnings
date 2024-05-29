de91fccecfe004fcc90097eb052bf9aadf9d76cf
Anirudh <aani.spark@gmail.com>
2024-05-29 18:13:43 +0530

    corrections

--- routeTable.tf
+++ routeTable.tf
@@ -2,12 +2,12 @@ resource "aws_route_table" "Tap-Public-Route" {
   vpc_id = aws_vpc.Tapplent.id
 
    route {
-    cidr_block = "10.0.1.0/24"
+    cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.Tapplent_igw.id
   }
 }
 
-resource "aws_route_table_association" "a" {
+resource "aws_route_table_association" "public" {
   subnet_id      = aws_subnet.Public-Subnet1.id
   route_table_id = aws_route_table.Tap-Public-Route.id
 }

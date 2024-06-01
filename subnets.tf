7a9bcfa031f2957975ab09dffe2d3c8383bf5089
Anirudh <aani.spark@gmail.com>
2024-06-01 15:53:42 +0530

    change in subnet association

--- subnets.tf
+++ subnets.tf
@@ -1,8 +1,10 @@
 resource "aws_subnet" "Public-Subnet1" {
+
+  count = 3
+
   vpc_id = aws_vpc.Tapplent.id
   cidr_block = var.aws_pubic_subnet
-  
-  count = 3
+ 
 
   tags = {
     Name = "Public-Subnet-${count.index}"
@@ -10,11 +12,22 @@ resource "aws_subnet" "Public-Subnet1" {
 }
 
 resource "aws_subnet" "Private-Subnet1" {
+    count = 3
     vpc_id = aws_vpc.Tapplent.id
     cidr_block = var.aws_private_subnet
-    count = 3
+
 
     tags = {
       Name = "Private-Subnet-${count.index}"
     }
+}
+
+resource "aws_route_table_association" "public" {
+  subnet_id      = aws_subnet.Public-Subnet1[count.index].id
+  route_table_id = aws_route_table.Tap-Public-Route.id
+}
+
+resource "aws_route_table_association" "private" {
+  subnet_id      = aws_subnet.Private-Subnet1[count.index].id
+  route_table_id = aws_route_table.Tap-Private-Route.id
 }

3dff75b11350e92346cfb2495b04e035290e8ab5
Anirudh <aani.spark@gmail.com>
2024-06-01 15:04:16 +0530

    using count parameter to craete 3 public and 3 private subnets in subnet.tf file

--- subnets.tf
+++ subnets.tf
@@ -1,17 +1,20 @@
 resource "aws_subnet" "Public-Subnet1" {
   vpc_id = aws_vpc.Tapplent.id
   cidr_block = var.aws_pubic_subnet
+  
+  count = 3
 
   tags = {
-    Name = "Public-Subnet1"
+    Name = "Public-Subnet-${count.index}"
   }
 }
 
 resource "aws_subnet" "Private-Subnet1" {
     vpc_id = aws_vpc.Tapplent.id
     cidr_block = var.aws_private_subnet
+    count = 3
 
     tags = {
-      Name = "Private-Subnet1"
+      Name = "Private-Subnet-${count.index}"
     }
 }

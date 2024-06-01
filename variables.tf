611240be1bc187799c41ca280026fd394fcf0a41
Anirudh <aani.spark@gmail.com>
2024-06-01 14:51:04 +0530

    added default values in few lines in variables.tf file

--- variables.tf
+++ variables.tf
@@ -1,7 +1,15 @@
-variable "aws_region" {}
+variable "aws_region" {
+    default = "ap-south-2"
+}
 
-variable "aws_vpcId" {}
+variable "aws_vpcId" {
+    default = "10.0.0.0/16"
+}
 
-variable "aws_pubic_subnet" {}
+variable "aws_pubic_subnet" {
+    default = "10.0.1.0/24"
+}
 
-variable "aws_private_subnet" {}
+variable "aws_private_subnet" {
+    default = "10.0.10.0/24"
+}
